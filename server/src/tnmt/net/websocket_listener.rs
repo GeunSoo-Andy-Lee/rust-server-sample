ab130975cebafa568c807f97820680d2c6f9ec1c
ownrisk <ownrisk@gmail.com>
2023-03-10 22:09:01 +0900

    web socket handshake callback 추가.

--- server/src/tnmt/net/websocket_listener.rs
+++ server/src/tnmt/net/websocket_listener.rs
@@ -1,8 +1,6 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{net::SocketAddr, time::Duration, cell::{RefCell, Ref}, sync::{Arc, Mutex}};
 
-use async_std::channel::Receiver;
-use futures_util::{StreamExt, SinkExt, stream::{SplitSink, SplitStream}, Stream};
-//use tokio::{net::{TcpStream, TcpListener}, task::JoinHandle, io::AsyncWriteExt};
+use futures_util::{StreamExt, SinkExt};
 use tokio::{task::JoinHandle, io::AsyncWriteExt, net::{TcpListener, TcpStream}};
 //use std::{net::{TcpStream, TcpListener}};
 //use tokio::{net::{ToSocketAddrs}};
@@ -44,7 +42,8 @@ impl WebSocketListener {
 
         while let Ok((stream, _)) = listener.accept().await {
             let peer = stream.peer_addr().expect("connected streams should have a peer address");
-            tokio::spawn(handle_accept(peer, stream));
+            let _ = handle_accept(peer, stream).await;
+            //tokio::spawn(handle_accept(peer, stream));
         }
 
         Ok(())
@@ -54,12 +53,29 @@ impl WebSocketListener {
 async fn handle_accept(peer: SocketAddr, stream: TcpStream) -> std::io::Result<()> {
     log::info!("connected from {}", peer);
 
-    if let Err(e) = handle_receive(peer, stream).await {
-        match e {
-            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
-            err => log::error!("Error processing connection: {}", err),
+    //let local_address = stream.local_addr();
+    //let peer_address = stream.peer_addr();
+
+    let path = RefCell::new("".to_string());
+    let headers_callback = {
+        |req: &Request, res: Response| -> Result<Response, ErrorResponse> {
+            //let header = req.headers().get("Authentication").unwrap();
+            path.replace(req.uri().path().to_string());
+            Ok(res)
         }
-    }
+    };
+
+    //let stream = accept_async(stream).await?;
+    let stream = accept_hdr_async(stream, headers_callback).await.unwrap();
+    println!("path: {}", path.borrow());
+
+    let _ = tokio::spawn(handle_receive(peer, stream));
+    //if let Err(e) = handle_receive(peer, websocket_stream).await {
+    //    match e {
+    //        Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+    //        err => log::error!("Error processing connection: {}", err),
+    //    }
+    //}
 
     Ok(())
 }
@@ -67,36 +83,23 @@ async fn handle_accept(peer: SocketAddr, stream: TcpStream) -> std::io::Result<(
 //type StreamResult = Result<Message, tokio_tungstenite::tungstenite::Error>;
 //SplitSink<WebSocketStream<tokio::net::TcpStream>, Result<Message, tokio_tungstenite::tungstenite::Error>>`
 
-async fn handle_receive(peer: SocketAddr, stream: TcpStream) -> Result<()> {
-    let local_address = stream.local_addr();
-    let peer_address = stream.peer_addr();
+async fn handle_receive(peer: SocketAddr, stream: WebSocketStream<TcpStream>) -> Result<()> {
 
-    let path = std::cell::RefCell::new("".to_string());
-    let headers_callback = |req: &Request, res: Response| -> Result<Response, ErrorResponse> {
-        println!("{:?}", req.uri());
-        path.replace(req.uri().path().to_string());
-        Ok(res)
-    };
+    let (mut write_stream, mut read_stream) = stream.split();
 
-    let websocket_stream = accept_async(stream).await?;
-    //let websocket_stream = accept_hdr_async(stream, headers_callback).await?;
-    //println!("path: {}", path.borrow());
+    let mut keepalive = tokio::time::interval(Duration::from_millis(1000));
 
-    let (mut ws_send, mut ws_recv) = websocket_stream.split();
-
-    let mut interval = tokio::time::interval(Duration::from_millis(1000));
-
-    //let socket = SocketImpl { read_stream: ws_recv, write_stream: ws_send };
+    //let x = read_stream
     // Echo incoming WebSocket messages and send a message periodically every second.
-
     loop {
         tokio::select! {
-            msg = ws_recv.next() => {
+            //  
+            msg = read_stream.next() => {
                 match msg {
                     Some(msg) => {
                         let msg = msg?;
                         if msg.is_text() ||msg.is_binary() {
-                            ws_send.send(msg).await?;
+                            write_stream.send(msg).await?;
                         } else if msg.is_close() {
                             break;
                         }
@@ -105,7 +108,7 @@ async fn handle_receive(peer: SocketAddr, stream: TcpStream) -> Result<()> {
                 }
             }
 
-            _ = interval.tick() => {
+            _ = keepalive.tick() => {
                 //let dummy = [].to_vec();
                 //ws_sender.send(Message::Ping(dummy)).await?;
                 //ws_sender.send(Message::Text("tick".to_owned())).await?;
@@ -119,31 +122,31 @@ async fn handle_receive(peer: SocketAddr, stream: TcpStream) -> Result<()> {
 
 
 
-struct WebSocket {
-    tx: SplitSink<WebSocketStream<TcpStream>, Message>,
-    //rx: SplitStream<WebSocketStream<TcpStream>>,
-}
-
-impl WebSocket {
-    async fn new(stream: TcpStream) -> std::io::Result<(Self, SplitStream<WebSocketStream<TcpStream>>)> {
-        let (tx, rx) = match accept_async(stream).await {
-            Ok(stream) => Ok(stream),
-            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to accept: {e}"))),
-        }?.split();
-
-        //Ok(Self { tx, rx })
-        Ok((Self { tx }, rx))
-    }
-
-    pub async fn close(&mut self) {
-        self.tx.close().await;
-    }
-
-    pub async fn send(&mut self, data: &[u8]) -> std::io::Result<()> {
-        match self.tx.send(Message::Binary(data.to_vec())).await {
-            Ok(_) => Ok(()),
-            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, format!("{e}"))),
-        }
-    }
-}
+//struct WebSocket {
+//    tx: SplitSink<WebSocketStream<TcpStream>, Message>,
+//    //rx: SplitStream<WebSocketStream<TcpStream>>,
+//}
+//
+//impl WebSocket {
+//    async fn new(stream: TcpStream) -> std::io::Result<(Self, SplitStream<WebSocketStream<TcpStream>>)> {
+//        let (tx, rx) = match accept_async(stream).await {
+//            Ok(stream) => Ok(stream),
+//            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to accept: {e}"))),
+//        }?.split();
+//
+//        //Ok(Self { tx, rx })
+//        Ok((Self { tx }, rx))
+//    }
+//
+//    pub async fn close(&mut self) {
+//        self.tx.close().await;
+//    }
+//
+//    pub async fn send(&mut self, data: &[u8]) -> std::io::Result<()> {
+//        match self.tx.send(Message::Binary(data.to_vec())).await {
+//            Ok(_) => Ok(()),
+//            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, format!("{e}"))),
+//        }
+//    }
+//}
 
